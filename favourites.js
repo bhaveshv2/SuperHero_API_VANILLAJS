90cebd3a43d11610304a067cd036a73080d9c747
bhaveshv2 <bhaveshv3024@gmail.com>
2020-08-07 23:01:58 +0530

    favourite page and script file done

---
+++ favourites.js
@@ -0,0 +1,66 @@
+var listFavourites = JSON.parse(localStorage.getItem('listFavourites'));
+if(listFavourites === null){
+    listFavourites = [];
+}
+
+var favourite = document.getElementById('favourite-superhero');
+
+if(listFavourites.length === 0){
+    favourite.innerHTML = '<div style="color:white;text-align:center">No one is your Favourite!</div>'
+}
+
+for(var i of listFavourites){
+    fetch('https://superheroapi.com/api.php/2772108476351146/'+i+'/image')
+    .then(response=>response.json())
+    .then(data=>{
+        createHero(data);
+    })
+    .catch(err=>console.log(err));
+}
+
+function createHero(data){
+    console.log(data);
+    var heroCard = document.createElement('div');
+    heroCard.className = 'fav-hero-card';
+
+    var heroImageContainer = document.createElement('div');
+    heroImageContainer.className = 'fav-hero-image-container';
+    var heroImage = document.createElement('img');
+    heroImage.src = data.url;
+    heroImageContainer.appendChild(heroImage);
+    heroCard.appendChild(heroImageContainer);
+
+    var heroName = document.createElement('div');
+    heroName.className = 'fav-hero-name-container';
+    heroName.innerHTML = data.name;
+    heroCard.appendChild(heroName);
+    
+    var favIcon = document.createElement('div');
+    favIcon.className = 'fav-icon'
+    var icon = document.createElement('img');
+    icon.id = 'icon';
+    icon.src = 'https://image.flaticon.com/icons/svg/3208/3208707.svg';
+    favIcon.appendChild(icon);
+    heroCard.appendChild(favIcon);
+
+    heroCard.setAttribute('id',data.id);
+
+    favourite.appendChild(heroCard);
+}
+
+favourite.onclick = function(e){
+    if(e.target.id === 'icon'){
+        e.target.parentNode.parentNode.remove();
+        
+        var heroId = e.target.parentNode.parentNode.getAttribute('id');
+        var j = listFavourites.indexOf(heroId);
+        listFavourites.splice(j,1);
+        console.log(listFavourites);
+    }
+
+    localStorage.setItem('listFavourites',JSON.stringify(listFavourites));
+
+    if(listFavourites.length===0){
+        favourite.innerHTML = '<div style="color:white;text-align:center">No one is your Favourite!</div>';
+    }
+}
